ea269a9656cc79c3d96c7e513374975a5a5d1e68
Rae Williams <raewilliams@gmail.com>
2017-03-02 16:29:11 -0500

    Updated main app function2

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-    io.WriteString(w, "Hello GCP Lets SPINCICD")
+    io.WriteString(w, "Hello GCP Lets Play")
 }
 
 func main() {
